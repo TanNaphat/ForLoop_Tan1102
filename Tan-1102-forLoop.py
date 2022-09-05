51015ca8c82f3af5a9cc8780f5724e0eefd16104
TanNaphat <112365248+TanNaphat@users.noreply.github.com>
2022-09-05 10:45:23 +0700

    Add files via upload

---
+++ Tan-1102-forLoop.py
@@ -0,0 +1,22 @@
+#Code 1
+var = 0
+for i in range(5):
+    var += 2
+    print(var)
+print("Goodbye!")
+
+#Code 2
+var = 10
+print("Hello!")
+for i in range(5):
+    print(var)
+    var -= 2
+
+#Code 3
+end = 6
+data = 0
+counter = 1
+for i in range(end):
+    data += counter
+    counter += 1
+print(data)
